e596eeed1eca67397653007c4318601ffd024090
barbaraurbanowicz <barbara.urbanowicz@microsoft.wsei.edu.pl>
2022-04-01 22:48:42 +0200

    CalculateStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -34,6 +34,17 @@ namespace GradeBook.GradeBooks
             else
             return 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count() < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+            else
+            base.CalculateStatistics();
+        }
     }
     }
 
